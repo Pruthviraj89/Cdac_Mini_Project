7f87c782a4f219d28f8459077cf8549104f90009
Pruthviraj89 <pruthviraj9mane@gmail.com>
2025-05-13 13:57:54 +0530

    updated 2.0

---
+++ BackEnd/src/controllers/Booking.js
@@ -0,0 +1,82 @@
+import { StatusCodes } from "http-status-codes";
+import { createDbConnection } from "../DbConfig/dbconfig.js";
+
+const conn = createDbConnection();
+
+// Create Booking
+export function createBooking(req, res) {
+    const data = req.body;
+    const qry = `INSERT INTO bookings (user_id, course_id, booking_date, status, payment_status) 
+                 VALUES (${data.user_id}, ${data.course_id}, '${data.booking_date}', '${data.status}', '${data.payment_status}')`;
+
+    conn.query(qry, (err, result) => {
+        if (err) {
+            console.log(err);
+            res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({ message: "Insertion failed" });
+        } else {
+            res.status(StatusCodes.OK).send({ message: "Booking created", id: result.insertId });
+        }
+    });
+}
+
+// Get All Bookings
+export function getAllBookings(req, res) {
+    const qry=`SELECT * FROM bookings`;
+    conn.query(qry, (err, result) => {
+        if (err) {
+            console.log(err);
+            res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({ message: "Fetching failed" });
+        } else {
+            res.status(StatusCodes.OK).send(result);
+        }
+    });
+}
+
+// Get Booking by ID
+export function getBookingById(req, res) {
+    const id = parseInt(req.params.id);
+    const qry= `SELECT * FROM bookings WHERE booking_id = ${id}`;
+    conn.query(qry , (err, result) => {
+        if (err) {
+            console.log(err);
+            res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({ message: "Error fetching booking" });
+        } else if (result.length === 0) {
+            res.status(StatusCodes.NOT_FOUND).send({ message: "Booking not found" });
+        } else {
+            res.status(StatusCodes.OK).send(result[0]);
+        }
+    });
+}
+
+// Update Booking
+export function updateBooking(req, res) {
+    const id = parseInt(req.params.id);
+    const data = req.body;
+    const qry = `UPDATE bookings SET 
+        user_id = ${data.user_id}, course_id = ${data.course_id}, booking_date = ${data.booking_date}, status = '${data.status}', 
+        payment_status = '${data.payment_status}'
+        WHERE booking_id = ${id}`;
+
+    conn.query(qry, (err) => {
+        if (err) {
+            console.log(err);
+            res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({ message: "Update failed" });
+        } else {
+            res.status(StatusCodes.OK).send({ message: "Booking updated" });
+        }
+    });
+}
+
+// Delete Booking
+export function deleteBooking(req, res) {
+    const id = parseInt(req.params.id);
+    const qry= `DELETE FROM bookings WHERE booking_id = ${id}`;
+    conn.query(qry, (err) => {
+        if (err) {
+            console.log(err);
+            res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({ message: "Deletion failed" });
+        } else {
+            res.status(StatusCodes.OK).send({ message: "Booking deleted" });
+        }
+    });
+}
---
+++ BackEnd/src/routes/YogaRoutes.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import { addUsers, deleteUsers, getUserById, getUsers, loginUser, putUsers } from '../controllers/Users.js';
+import { getCourse } from '../controllers/Course.js';
+
+
+const  YogaRouter=express.Router();
+
+
+//posting blog for 1st time
+YogaRouter.post("/",addUsers);
+YogaRouter.post("/login",loginUser)
+YogaRouter.get("/",getUsers);
+YogaRouter.get("/:id",getUserById);
+//update
+YogaRouter.put("/:id",putUsers);
+YogaRouter.delete("/:id",deleteUsers);
+
+
+
+
+
+
+export default YogaRouter;
